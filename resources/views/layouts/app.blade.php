a646f69821d182dce04ea9a5655bbad42f8e72b8
Expedit206 <mciagnessi@gmail.com>
2024-09-16 03:44:23 +0100

    maeit better

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -36,7 +36,7 @@
             <!-- Page Heading -->
             @isset($header)
                 <header class="bg-gray-600 shadow">
-                    <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
+                    <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-7">
                         {{ $header }}
                     </div>
                 </header>
