e13bbd6703f1ee5775d753744a68650b4f7c1252
Expedit206 <mciagnessi@gmail.com>
2024-09-16 06:49:42 +0100

    pojacker

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -50,7 +50,7 @@
         </div>
         <p>
 
-            <a href="https://api.whatsapp.com/send?text={{ urlencode('Check%20out%20this%20amazing%20app:%20https://potjacker-a78c3d041f48.herokuapp.com/') }}" target="_blank" class="fixed text-green-500 top-[90vh] left-[90vw]"> <i class="fas fa-share-alt text-2xl"></i></a>
+            <a href="https://api.whatsapp.com/send?text={{ urlencode('Check%20out%20this%20amazing%20app:%20https://potjacker-a78c3d041f48.herokuapp.com/') }}" target="_blank" class="fixed text-green-500 top-[90vh] left-[90vw]"> Partage<i class="fas fa-share-alt text-2xl"></i></a>
         </p>
 
     </body>
