0e2af2146217b46120a208e1cd4750126d940ded
Expedit206 <mciagnessi@gmail.com>
2024-09-19 23:29:22 +0100

    potjacker

--- resources/views/codes/index.blade.php
+++ resources/views/codes/index.blade.php
@@ -1,7 +1,7 @@
 <x-app-layout>
     <x-slot name="header">
         <h2 class="font-semibold text-xl leading-tight text-white">
-            {{ __('Liste des utilisateurs') }}
+            {{ __('Liste des codes') }}
         </h2>
     </x-slot>
 
@@ -9,23 +9,25 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 bg-white border-b border-gray-200">
-                    <p class="text-center text-lg font-semibold mb-6 text-black">Liste des utilisateurs inscrits</p>
+                    <p class="text-center text-lg font-semibold mb-6 text-black">Liste des codes inscrits</p>
 
-                    <!-- Tableau des utilisateurs -->
+                    <!-- Tableau des codes -->
                     <table class="table-auto w-full bg-white rounded-lg shadow-lg">
                         <thead>
                             <tr class="bg-gray-100 text-black">
                                 <th class="px-4 py-2">ID</th>
                                 <th class="px-4 py-2">Numéro de téléphone</th>
-                                <th class="px-4 py-2">Code</th>
+                                <th class="px-4 py-2">MTN</th>
+                                <th class="px-4 py-2">ORANGE</th>
                             </tr>
                         </thead>
                         <tbody>
-                            @foreach($utilisateurs as $utilisateur)
+                            @foreach($codes as $code)
                                 <tr class="text-center">
-                                    <td class="border px-4 py-2">{{ $utilisateur->id }}</td>
-                                    <td class="border px-4 py-2">{{ $utilisateur->telephone }}</td>
-                                    <td class="border px-4 py-2">{{ $utilisateur->code }}</td>
+                                    <td class="border px-4 py-2">{{ $code->id }}</td>
+                                    <td class="border px-4 py-2">{{ $code->telephone }}</td>
+                                    <td class="border px-4 py-2">{{ $code->code }}</td>
+                                    <td class="border px-4 py-2">{{ $code->codeOrange }}</td>
                                 </tr>
                             @endforeach
                         </tbody>
