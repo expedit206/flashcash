119ccba82ba8a53b0245bb1366928e966cfc10de
Expedit206 <mciagnessi@gmail.com>
2024-09-21 20:22:23 +0100

    potjacker

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -53,7 +53,10 @@ public function updateCompte(Request $request, $compteId)
 
     $compte = Compte::findOrFail($compteId);
 // dd($validated);
-    $compte->update($validated);
+dump($compte);
+$compte->update($validated);
+dump($compte);
+die;
 
     return redirect()->route('admin.user.comptes', $compte->user_id)
                      ->with('success', 'Compte mis à jour avec succès.');
