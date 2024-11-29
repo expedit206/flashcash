c7aaef4b6b23fcaf403c468acfbd2b57066ae6f8
Expedit206 <mciagnessi@gmail.com>
2024-11-29 20:06:28 +0100

    controller name sapce

--- app/Http/Controllers/EpargneController.php
+++ app/Http/Controllers/EpargneController.php
@@ -5,6 +5,8 @@ namespace App\Http\Controllers;
 use App\Http\Controllers\Controller;
 use App\Models\Epargne;
 use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
 
 class EpargneController extends Controller
 {
