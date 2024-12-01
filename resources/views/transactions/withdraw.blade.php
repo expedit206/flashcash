30b704676f825372c39118101b701968fd78c975
Expedit206 <mciagnessi@gmail.com>
2024-12-01 12:22:15 +0100

    withdraw ok

--- resources/views/transactions/withdraw.blade.php
+++ resources/views/transactions/withdraw.blade.php
@@ -8,7 +8,7 @@
         <label for="amount">Montant :</label>
         <input type="number" name="amount" required>
 
-        <button type="submit">Depot</button>
+        <button type="submit" class='bg-green-600'>Depot</button>
     </form>
     @if (session('error'))
         <div>{{ session('error') }}</div>
