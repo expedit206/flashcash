25723d53e323e4be5b533303723cf66cd5073c91
Expedit206 <mciagnessi@gmail.com>
2024-12-10 14:39:44 +0100

    modification de dat

--- resources/views/produit_user/index.blade.php
+++ resources/views/produit_user/index.blade.php
@@ -79,7 +79,7 @@
             </div>
             <div>
                 <p class="mb-1 font-bold text-gray-900">Revenu quotidien:</p>
-                <span>{{ number_format($produit->gainJ, 2) }} XAF</span>
+                <span>{{ number_format($produit->montant*$produit->rendement/100, 2) }} XAF</span>
             </div>
             <div>
                 {{-- @dd($produit) --}}
