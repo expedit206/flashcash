f0348255b05faf1aa4cc6c0fc0d3f5096f99aea2
Expedit206 <mciagnessi@gmail.com>
2024-12-17 15:41:40 +0100

    build pk

--- app/Http/Controllers/TransactionController.php
+++ app/Http/Controllers/TransactionController.php
@@ -114,7 +114,7 @@ class TransactionController extends Controller
             'phone.digits_between:9,15' => 'Le numéro de téléphone doit contenir exactement 9 caractères.',
             'amount.required' => 'Le montant est requis.',
             'amount.numeric' => 'Le montant doit être un nombre.',
-            'amount.min' => 'Le montant minimun de depôt est de 100 XAF.',
+            'amount.min' => 'Le montant minimun de depôt est de 1000 XAF.',
             'provider.required' => 'Le fournisseur est requis.',
         ]);
         // Créer une instance de Collect pour le retrait
